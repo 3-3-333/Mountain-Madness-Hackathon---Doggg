e30a803416b40c705e03851244f0fee22546c38f
XQQ0504 <2691832730@qq.com>
2022-04-30 12:00:46 -0700

    Lietener!!!!

--- Hackathon/hackat/src/main/java/org/doggg/Listener.java
+++ Hackathon/hackat/src/main/java/org/doggg/Listener.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class Listener implements KeyListener{
     GameFrame gf;
+    boolean up,down,right,left,enter;
     public Listener(GameFrame gf) {
         this.gf = gf;
     }
@@ -17,11 +18,48 @@ public class Listener implements KeyListener{
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
-        
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_UP){
+            up = true;
+        }
+        if(key == KeyEvent.VK_DOWN){
+            down = true;
+        }
+        if(key == KeyEvent.VK_RIGHT){
+            right = true;
+        }
+        if(key == KeyEvent.VK_LEFT){
+            left = true;
+        }
+        if(key == KeyEvent.VK_LEFT){
+            left = true;
+        }
+        if(key == KeyEvent.VK_ENTER){
+            enter = true;
+        }
     }
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_UP){
+            up = false;
+        }
+        if(key == KeyEvent.VK_DOWN){
+            down = false;
+        }
+        if(key == KeyEvent.VK_RIGHT){
+            right = false;
+        }
+        if(key == KeyEvent.VK_LEFT){
+            left = false;
+        }
+        if(key == KeyEvent.VK_LEFT){
+            left = false;
+        }
+        if(key == KeyEvent.VK_ENTER){
+            enter = false;
+        }
         
     }
 }
