e29e0afaaca4a5a76343c5e4bae32380c6c52ab2
ida8 <isaac_ding@sfu.ca>
2022-04-30 15:23:37 -0700

    edited clotheschooser

--- Hackathon/hackat/src/main/java/org/doggg/ClothesChooser.java
+++ Hackathon/hackat/src/main/java/org/doggg/ClothesChooser.java
@@ -1,5 +1,6 @@
 package org.doggg;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -8,16 +9,27 @@ public class ClothesChooser{
         up=new ArrayList<ClothesNode>();
         r=new Random();
     }
-    public void MakeList()){
+    public void MakeList(){
         up.add(new ClothesNode("Up1", "resources/Up1.png"));
         up.add(new ClothesNode("Up2", "resources/Up2.png"));
         for(int i=1;i<6;i++){
             bodysuit.add(new ClothesNode("bodysuit"+i, 
                  "resources/bodysuit"+i+".png"));
         }
+        down=new ClothesNode("down1", "resources/Up1.png");
+        body=new ClothesNode("body", "resources/body.png");
     }
-    public void randomSet(){
-        
+    public void randomSet(Graphics2D g2){
+        g2.drawImage(body.getImage(), 1, 1, 1668, 2224, null);
+        int isBodySuit=r.nextInt(0, 2);
+        if(isBodySuit==1){
+            int index=r.nextInt(0,bodysuit.size());
+            ClothesNode bodysuitPic=bodysuit.get(index+1);
+
+        }else{
+            int upIndex=r.nextInt(0,up.size());
+            ClothesNode upPic=up.get(upIndex+1);
+        }
     }
     private ArrayList<ClothesNode> up;
     ClothesNode down;
