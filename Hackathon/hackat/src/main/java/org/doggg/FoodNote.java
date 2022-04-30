f6cdbad6b61a85f3ed0a74cbdf8ca51c57bd82f3
Candice <390347312@qq.com>
2022-04-29 22:55:51 -0700

    enter FoodNode

---
+++ Hackathon/hackat/src/main/java/org/doggg/FoodNote.java
@@ -0,0 +1,10 @@
+package org.doggg;
+
+public class FoodNode extend ItemNode{
+    private String name;
+    private BufferedImage entity;
+    public FoodNode(String name, String path){
+        super(name,path);
+    }
+
+}
