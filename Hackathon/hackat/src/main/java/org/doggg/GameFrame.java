442da116625bbbeda61adcafed6bc26bccd8da75
XQQ0504 <2691832730@qq.com>
2022-04-29 23:08:45 -0700

    add paint funciton

--- Hackathon/hackat/src/main/java/org/doggg/GameFrame.java
+++ Hackathon/hackat/src/main/java/org/doggg/GameFrame.java
@@ -1,7 +1,14 @@
 package org.doggg;
 import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JPanel implements Runnable{
     public void run(){};
+    public void paint(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+    }
+
 }
 
