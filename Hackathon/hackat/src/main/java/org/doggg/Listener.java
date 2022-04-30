77d1d403973f5896568f263bca3d9bced77bbf54
XQQ0504 <2691832730@qq.com>
2022-04-30 10:59:24 -0700

    Revert "Listener"
    This reverts commit fe19206d9328d8ebfa7fac0feb6493b5d766f4fd.

--- Hackathon/hackat/src/main/java/org/doggg/Listener.java
+++ Hackathon/hackat/src/main/java/org/doggg/Listener.java
@@ -1,57 +1,8 @@
 package org.doggg;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.IOException;
 
-public class Listener implements KeyListener{
+public class Listener {
     GameFrame gf;
-    boolean up,down,right,left,enter;
     public Listener(GameFrame gf) {
         this.gf = gf;
-    }
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // Useless
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        int key = e.getKeyCode();
-        if(key == KeyEvent.VK_UP){
-            up=true;
-        }
-        if(key == KeyEvent.VK_DOWN){
-            down = true;
-        }
-        if(key == KeyEvent.VK_RIGHT){
-            right=true;
-        }
-        if(key == KeyEvent.VK_LEFT){
-            left = true;
-        }
-        if(key == KeyEvent.VK_ENTER){
-            enter = true;
-        }
-    }
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        int key = e.getKeyCode();
-        if(key == KeyEvent.VK_UP){
-            up=false;
-        }
-        if(key == KeyEvent.VK_DOWN){
-            down = false;
-        }
-        if(key == KeyEvent.VK_RIGHT){
-            right=false;
-        }
-        if(key == KeyEvent.VK_LEFT){
-            left = false;
-        }
-        if(key == KeyEvent.VK_ENTER){
-            enter = false;
-        }
     }
 }
