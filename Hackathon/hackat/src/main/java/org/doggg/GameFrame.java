24c3d4f8f97c8bd01c97953380fbf6a6dc224f5d
mathewtse <88808907+mathewtse@users.noreply.github.com>
2022-04-30 11:08:46 -0700

    changed gameframe

--- Hackathon/hackat/src/main/java/org/doggg/GameFrame.java
+++ Hackathon/hackat/src/main/java/org/doggg/GameFrame.java
@@ -8,8 +8,14 @@ import java.io.IOException;
 
 public class GameFrame extends JPanel implements Runnable {
 
-    FoodNode icecream = new FoodNode("icecream", "resources/Food1_24x24.png");
-    Image icecreamImage = icecream.getImage();
+    //public Image food = ImageIO.read(getClass().getResourceAsStream("resources/Food1_24x24.png"));
+    FoodNode icecream=new FoodNode("icecream", "resources/Food1_24x24.png");
+    Image icecreamImage=icecream.getImage();
+
+    FoodNode bread = new FoodNode("bread","resources/Food2_24x24.png");
+    Image breadImage = bread.getImage();
+
+    // 我的相对路径有问题 换成绝对路径以后是可以正常显示的
     private int frame_speed = 60;
     private int rows, colm, cellSize, width, height;
     private Thread gameThread;
@@ -31,10 +37,31 @@ public class GameFrame extends JPanel implements Runnable {
         this.setFocusable(true);
     }
 
+
+    
     public void beginThread() {
         gameThread = new Thread(this);
         gameThread.start();
     }
+
+    //start of new function
+    private void titleScreen(Graphics2D g2) {
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        Font font1 = new Font("Sans Serif", Font.PLAIN, 96);
+        Font font2 = new Font("Sans Serif", Font.PLAIN, 36);
+        int text1Length = (int)
+            g2.getFontMetrics(font1).stringWidth("indecisive.io");
+        int text2Length = (int)
+            g2.getFontMetrics(font2).stringWidth("press space to continue");
+        g2.setFont(font1);
+        g2.drawString("indecisive.io",
+            (width/2)-(text1Length/2),height/2-96);
+        g2.setFont(font2);
+        g2.drawString("press space to continue",
+             width/2-text2Length/2,height/2+40);
+    }
+
 
     @Override
     public void run() {
@@ -53,14 +80,15 @@ public class GameFrame extends JPanel implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
+    };
 
     @Override
     public void paint(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(icecreamImage, 1, 1, 48, 48, null);
-
+        //g2.drawImage(icecreamImage, 1, 1, 48, 48, null);
+        //g2.drawImage(breadImage,480,480,48,48,null);
+        titleScreen(g2);
     }
 
     public void runGame() {
@@ -68,3 +96,4 @@ public class GameFrame extends JPanel implements Runnable {
     }
 
 }
+
