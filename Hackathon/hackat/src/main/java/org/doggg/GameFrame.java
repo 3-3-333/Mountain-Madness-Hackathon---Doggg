057ac9b6385f2134c707aaae2ef9f2ce677dfb3a
XQQ0504 <2691832730@qq.com>
2022-04-29 23:10:37 -0700

    some tiny motified

--- Hackathon/hackat/src/main/java/org/doggg/GameFrame.java
+++ Hackathon/hackat/src/main/java/org/doggg/GameFrame.java
@@ -1,10 +1,11 @@
 package org.doggg;
 import javax.swing.JPanel;
-import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JPanel implements Runnable{
+    @Override
     public void run(){};
+    @Override
     public void paint(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
