a79e1ffd53a9c001ab6488d51ddb3b69c84241a9
XQQ0504 <2691832730@qq.com>
2022-04-30 11:53:16 -0700

    listener

--- Hackathon/hackat/src/main/java/org/doggg/Listener.java
+++ Hackathon/hackat/src/main/java/org/doggg/Listener.java
@@ -1,8 +1,27 @@
 package org.doggg;
 
-public class Listener {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+
+public class Listener implements KeyListener{
     GameFrame gf;
     public Listener(GameFrame gf) {
         this.gf = gf;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        // Donttttttttt Use 
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
